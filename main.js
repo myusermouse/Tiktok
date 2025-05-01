a6483db43601ea0e42c6e4ed88e6ceed9642e7cf
myusermouse <mdrahulsarkar5525@gmail.com>
2025-05-01 16:24:00 +0600

    update

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-// phone or email when click
 function redirectToPhoneEmailLogin() {
   // Replace 'phone-email-login.html' with your actual file name
   window.location.href = '/Pages/Phone or email/phone.html';
+}
+
+function redirectEmailLogin() {
+  // Replace 'phone-email-login.html' with your actual file name
+  window.location.href = '/Pages/Gmail/gmail.html';
 }
